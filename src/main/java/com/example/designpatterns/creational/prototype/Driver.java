02f71f4a7e028ae1663d8ec13ee2baaa62efb48c
VAMSI KARUTURI <karuturi-sai-naga.vamsi-krishna@siemens.com>
2023-07-01 11:33:05 +0530

    prototype dp

---
+++ src/main/java/com/example/designpatterns/creational/prototype/Driver.java
@@ -0,0 +1,17 @@
+package com.example.designpatterns.creational.prototype;
+
+public class Driver {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(1,"Vamsi Krishna",24,10000000);
+
+        System.out.println(employee1);
+        System.out.println(employee1.hashCode());
+
+
+
+
+        Employee employee2 = (Employee) employee1.clone();
+        System.out.println(employee2);
+        System.out.println(employee2.hashCode());
+    }
+}
---
+++ src/main/java/com/example/designpatterns/creational/prototype/Prototype.java
@@ -0,0 +1,5 @@
+package com.example.designpatterns.creational.prototype;
+
+public interface Prototype {
+    Prototype clone();
+}
