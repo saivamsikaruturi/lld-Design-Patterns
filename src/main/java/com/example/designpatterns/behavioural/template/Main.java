545f2f842e701a82a71c71edfc9bdde12d4ed6a7
VAMSI KARUTURI <karuturi-sai-naga.vamsi-krishna@siemens.com>
2023-07-27 16:53:31 +0530

    template dp

---
+++ src/main/java/com/example/designpatterns/behavioural/template/Main.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.behavioural.template;
+
+public class Main {
+    public static void main(String[] args) {
+        PizzaMaker vegPizzaMaker = new VegPizzaMaker();
+        vegPizzaMaker.makePizza();
+
+        System.out.println("------------------------------");
+        PizzaMaker nvPizzaMaker = new NonVegPizzaMaker();
+        nvPizzaMaker.makePizza();
+
+    }
+}
---
+++ src/main/java/com/example/designpatterns/behavioural/template/VegPizzaMaker.java
@@ -0,0 +1,12 @@
+package com.example.designpatterns.behavioural.template;
+
+public class VegPizzaMaker extends PizzaMaker {
+    @Override
+    public void prepareIngredients() {
+        System.out.println("Preparing mushroom, tomato slices, onions, and fresh basil leaves.");
+    }
+    @Override
+    public void addToppings() {
+        System.out.println("Adding mozzerella cheese and tomato sauce along with ingredients to crust.");
+    }
+}
