0181a6d2b771671111ed52f6f0f97f89e9935e4b
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-11-26 20:36:42 +0530

    prototype dp

---
+++ src/main/java/com/example/designpatterns/creational/prototype/Movie.java
@@ -0,0 +1,34 @@
+package com.example.designpatterns.creational.prototype;
+
+public class Movie implements Cloneable{
+    private String title;
+    private String director;
+    private String language;
+    private String duration;
+
+    public Movie(String title, String director, String language, String duration) {
+        this.title = title;
+        this.director = director;
+        this.language = language;
+        this.duration = duration;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    protected Movie clone() throws CloneNotSupportedException {
+        return (Movie) super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", director='" + director + '\'' +
+                ", language='" + language + '\'' +
+                ", duration='" + duration + '\'' +
+                '}';
+    }
+}
