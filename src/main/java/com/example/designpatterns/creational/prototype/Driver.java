a1558df582128c888709b8138e5b94acb4a7a00b
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-11-26 20:13:50 +0530

    prototype dp

--- src/main/java/com/example/designpatterns/creational/prototype/Driver.java
+++ src/main/java/com/example/designpatterns/creational/prototype/Driver.java
@@ -1,7 +1,7 @@
 package com.example.designpatterns.creational.prototype;
 
 public class Driver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         Employee employee1 = new Employee(1,"Vamsi Krishna",24,10000000);
 
         System.out.println(employee1);
@@ -13,5 +13,12 @@ public class Driver {
         Employee employee2 = (Employee) employee1.clone();
         System.out.println(employee2);
         System.out.println(employee2.hashCode());
+
+        Movie movie = new Movie("Devara","Siva","Telugu","2:30");
+        Movie kannadaMovie = movie.clone();
+        kannadaMovie.setLanguage("Kannada");
+
+        System.out.println(movie);
+        System.out.println(kannadaMovie);
     }
 }
