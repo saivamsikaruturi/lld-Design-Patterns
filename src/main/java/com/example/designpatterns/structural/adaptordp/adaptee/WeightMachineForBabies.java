9762bc2f7f65f4b6771d3108e2ee6ebde0c88678
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-11-30 19:43:47 +0530

    adaptor dp weight conversion

--- src/main/java/com/example/designpatterns/structural/adaptordp/adaptee/WeightMachineForBabies.java
+++
@@ -1,9 +0,0 @@
-package com.example.designpatterns.structural.adaptordp.adaptee;
-
-public class WeightMachineForBabies implements WeightMachine{
-    @Override
-    public double getWeightInPound() {
-        return 28;
-    }
-}
-
