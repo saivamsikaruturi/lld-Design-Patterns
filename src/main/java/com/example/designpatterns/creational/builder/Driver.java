1d3852cd8400244a3bb42bbf70ec14b009412202
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-11-30 20:14:37 +0530

    builder dp

--- src/main/java/com/example/designpatterns/creational/builder/Driver.java
+++ src/main/java/com/example/designpatterns/creational/builder/Driver.java
@@ -4,13 +4,12 @@ import com.example.designpatterns.creational.builder.AmazonOrders.Order;
 
 public class Driver {
     public static void main(String[] args) {
-        User user = new User.UserBuilder("LavaKumar","T").build();
+        User user = new User.UserBuilder("Vamsi","Krishna").build();
         System.out.println(user);
 
 
-        user = new User.UserBuilder("LavaKumar","T").setAge(24).setPhone("9890098900").build();
+        user = new User.UserBuilder("Vamsi","Krishna").setAge(24).setPhone("9999999999").build();
         System.out.println(user);
-        Car car = new Car.CarBuilder(3,"ab").build();
 
          Order order = new Order.OrderBuilder("1",1000.0,"UPI","Hyd","Hyd","Laptop","144167")
                  .setDiscountCode("VAMSI250").build();
