884ab3dd1defa57106b25776711f7ae7dc7d74ac
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-12-01 17:51:05 +0530

    facade dp FileCompression

--- src/main/java/com/example/designpatterns/structural/facade/OggCompressionCodec.java
+++
@@ -1,9 +0,0 @@
-package com.example.designpatterns.structural.facade;
-
-public class OggCompressionCodec implements CompressionCodec{
-
-    @Override
-    public String getType() {
-        return "ogg";
-    }
-}
